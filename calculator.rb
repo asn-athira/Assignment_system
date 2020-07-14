31257d6652956c0a44e444c895343072f5144ebb
asn-athira <athira@rightsolutions.ae>
2020-07-14 12:52:27 +0530

    New line added on two files

--- calculator.rb
+++ calculator.rb
@@ -10,3 +10,4 @@ puts "The first number divided by the second number is: #{first_number.to_f / se
 puts "The first number subtracted from the second number is: #{second_number.to_f - first_number.to_f}"
 puts "The first number added to the second number is: #{second_number.to_f + first_number.to_f}"
 puts "The first number mod the second number is: #{first_number.to_f % second_number.to_f}"
+puts "Welcome......"
